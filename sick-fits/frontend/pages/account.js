2d99f2698908bd947e5457f7e0d4db453e6361d8
Brandon Williams <bmwill@outlook.com>
2021-02-04 11:11:16 -0700

    wip

--- sick-fits/frontend/pages/account.js
+++ sick-fits/frontend/pages/account.js
@@ -1,7 +1,9 @@
+import Page from '../components/Page';
+
 export default function AccountPage() {
   return (
-    <div>
+    <Page>
       <p>Account</p>
-    </div>
+    </Page>
   );
 }
--- sick-fits/frontend/pages/orders.js
+++ sick-fits/frontend/pages/orders.js
@@ -1,7 +1,9 @@
+import Page from '../components/Page';
+
 export default function OrdersPage() {
   return (
-    <div>
+    <Page>
       <p>Orders</p>
-    </div>
+    </Page>
   );
 }
