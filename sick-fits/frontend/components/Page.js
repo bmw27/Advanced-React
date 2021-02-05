58ba072f8fe4137f695dd176f84404a2908185f8
Brandon Williams <bmwill@outlook.com>
2021-02-04 18:46:37 -0700

    wip

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -9,7 +9,6 @@ const GlobalStyles = createGlobalStyle`
     font-weight: normal;
     font-style: normal;
   }
-
   html {
     --red: #ff1234;
     --black: #393939;
@@ -20,13 +19,11 @@ const GlobalStyles = createGlobalStyle`
     --offWhite: #ededed;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0, 0, 0, 0.9);
-
     box-sizing: border-box;
     *, *:before, *:after {
       box-sizing: inherit;
     }
   }
-
   body {
     font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     padding: 0;
@@ -34,16 +31,13 @@ const GlobalStyles = createGlobalStyle`
     font-size: 1.5rem;
     line-height: 2;
   }
-
   a {
     text-decoration: none;
     color: var(--black);
   }
-
   a:hover {
     text-decoration: underline
   }
-
   button {
     font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
