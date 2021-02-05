efdb1d8be4f934763e40ade41e7dd1ac3f86edb3
Brandon Williams <bmwill@outlook.com>
2021-02-04 18:15:52 -0700

    wip

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,11 +1,29 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  background: red;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">Sick fits</Link>
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
       </div>
 
       <div className="sub-bar">
