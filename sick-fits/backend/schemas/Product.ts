11b98c28b12609662f86a5de75f21c117b88226e
Brandon Williams <bmwill@outlook.com>
2021-02-17 21:12:10 -0700

    wip

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -1,5 +1,5 @@
 import { list } from '@keystone-next/keystone/schema';
-import { text, select, integer } from '@keystone-next/fields';
+import { text, select, integer, relationship } from '@keystone-next/fields';
 
 export const Product = list({
   // access:
@@ -8,6 +8,19 @@ export const Product = list({
     description: text({
       ui: {
         displayMode: 'textarea',
+      },
+    }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: {
+          fields: ['image', 'altText'],
+        },
+        inlineEdit: {
+          fields: ['image', 'altText'],
+        },
       },
     }),
     status: select({
@@ -23,6 +36,5 @@ export const Product = list({
       },
     }),
     price: integer(),
-    // TODO: photo
   },
 });
