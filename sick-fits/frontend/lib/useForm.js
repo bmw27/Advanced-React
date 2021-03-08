4fcc75aa5ef5bb20e80450b80685b3295e30b6e6
Brandon Williams <bmwill@outlook.com>
2021-03-07 17:08:00 -0700

    wip

---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type, files } = e.target;
+
+    console.log(files);
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    if (type === 'file') {
+      value[0] = files;
+    }
+
+    setInputs({
+      // Copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+
+    setInputs(blankState);
+  }
+
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
